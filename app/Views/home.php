3dee4689e6f7ddf55ef016a91003112d317e54c1
Glauco Pereira <glaucoadm@gmail.com>
2025-03-12 10:58:53 -0300

    tela de login

--- app/Views/home.php
+++ app/Views/home.php
@@ -5,7 +5,7 @@
 <main class="container">
     <h1 class="mt-5">Home!</h1>
     
-    <h3>Estamos na aula XX: <a href="#"> : : : | Link | : : : </a></h3>
+    <h3>Estamos na aula 36: <a href="https://www.youtube.com/watch?v=iFpF2nuHo70&list=PLyugqHiq-SKegiaCBJ4XaFp-yr87oFKts&index=36"> : : : | Link | : : : </a></h3>
 
 </main>
 
