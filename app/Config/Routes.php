bf5df717b2f05755711e1f84cb1890e02cd2f747
Glauco Pereira <glaucoadm@gmail.com>
2025-03-15 13:35:30 -0300

    Controle de entrada

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,3 +9,10 @@ $routes->get('/', 'Home::index', ['as' => 'home']);
 $routes->get('/login', 'Login::index', ['as' => 'login']);
 $routes->post('/login', 'Login::store', ['as' => 'login.store']);
 $routes->get('/login/destroy', 'Login::destroy', ['as' => 'login.destroy']);
+
+$routes->get('/register', 'Register::index', ['as' => 'register']);
+$routes->post('/register/store', 'Register::store', ['as' => 'register.store' ]);
+
+$routes->group('admin',['filter' => 'auth'], function ($routes) {
+    $routes->get('private', 'RoutePrivate::index', ['as' => 'private']);
+});
---
+++ app/Views/private.php
@@ -0,0 +1,9 @@
+<?php echo $this->extend('master') ?>
+
+<?php echo $this->section('content') ?>
+
+<main class="container">
+    <h1 class="mt-5">Private!</h1>
+</main>
+
+<?php echo $this->endSection() ?>
